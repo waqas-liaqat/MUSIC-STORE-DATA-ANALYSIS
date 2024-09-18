f5e4d815824205c402acac6976bf16140764f75f
Muhammad Waqas <waqasliaqat630@gmail.com>
2024-09-18 10:58:46 +0500

    Update analysis_code_file.sql

--- analysis_code_file.sql
+++ analysis_code_file.sql
@@ -92,11 +92,6 @@ ORDER BY miliseconds DESC;
 
 /* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
 
-/* Steps to Solve: First, find which artist has earned the most according to the InvoiceLines. Now use this artist to find 
-which customer spent the most on this artist. For this query, you will need to use the Invoice, InvoiceLine, Track, Customer, 
-Album, and Artist tables. Note, this one is tricky because the Total spent in the Invoice table might not be on a single product, 
-so you need to use the InvoiceLine table to find out how many of each product was purchased, and then multiply this by the price
-for each artist. */
 
 WITH best_selling_artist AS (
 	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
